505a358e93fad7cacbc441d7bb5b8bde82609d88
vadimbasco <vadim.basco@gmail.com>
2016-11-23 21:31:27 +0400

    templates working copy 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
-var handlebars = require('handlebars');
 var gulp = require('gulp');
+var handlebars = require('gulp-compile-handlebars');
 
-gulp.task('build', function () {
+gulp.task('default', function () {
+
+	var templateData = {
+			firstName: 'Kaanon'
+		},
+		options = {
+			ignorePartials: true, //ignores the unknown footer2 partial in the handlebars template, defaults to false
+			partials : {
+				footer : '<footer>the end</footer>'
+			},
+			batch : ['./src/parts'],
+			helpers : {
+				capitals : function(str){
+					return str.toUpperCase();
+				}
+			}
+		};
+
+	return gulp.src('src/template.html')
+		.pipe(handlebars(templateData, options))
+		.pipe(gulp.dest('dist'));
 });
